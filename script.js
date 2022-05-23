26c7f7423bfde2d07c27c58fc764303d91d4ed34
Alexandros Charangionis <alexandroscharangionis@gmail.com>
2022-05-23 13:52:55 +0300

    added on/off toggle function for form appearance

--- script.js
+++ script.js
@@ -9,24 +9,25 @@ const bookTitle = document.getElementById("book_title");
 const bookAuthor = document.getElementById("book_author");
 const bookPages = document.getElementById("book_pages");
 const submitBook = document.getElementById("submit");
+const formWrapper = document.getElementById("wrapper");
 let myLibrary = [];
 
 // Book object constructor
-function Book(title, author, pages, read) {
+const Book = function (title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-}
+};
 
 // Create new object for new book entry and add to library array
-function addBookToLibrary(title, author, pages, read) {
+const addBookToLibrary = function (title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
-}
+};
 
 // Display book cards on page from library array
-function displayBooks() {
+const displayBooks = function () {
   const books = document.querySelector(".library");
 
   // Loops over library array and creates a card for each array item
@@ -54,7 +55,18 @@ function displayBooks() {
       card.appendChild(para);
     }
   });
-}
+};
+const addNewBook = function () {
+  bookForm.classList.toggle("book_form-active");
+  formWrapper.classList.toggle("form_wrapper");
+};
+
+closeBtn.addEventListener("click", () => {
+  formWrapper.classList.toggle("form_wrapper");
+  bookForm.classList.toggle("book_form-active");
+});
+
+addBtn.addEventListener("click", addNewBook);
 
 addBookToLibrary("Klara and the Sun", "Kazuo Ishiguro", 423);
 addBookToLibrary("Harry Potter", "J.K. Rowling", 500);
