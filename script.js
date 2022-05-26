4450c658cc03efe6e3b75098790ffb4a74579a6f
Alexandros Charangionis <alexandroscharangionis@gmail.com>
2022-05-26 13:59:36 +0300

    created delete card & library object(index) functionality)

--- script.js
+++ script.js
@@ -61,14 +61,36 @@ const addBookToLibrary = function (title, author, pages) {
   displayBooks();
 };
 
+const deleteBook = function () {
+  const index = myLibrary.indexOf();
+};
+
 const displayBooks = function () {
   // Loop through library array.
-  myLibrary.forEach((item) => {
+  myLibrary.forEach((item, index) => {
     // If current item(object) doesn't have displayed property to 'true', go on.
     if (item.displayed === false) {
       const card = document.createElement("div");
+      const deleteBtn = document.createElement("button");
+      deleteBtn.classList.add("deleteBtn");
+      deleteBtn.textContent = "X";
+      card.appendChild(deleteBtn);
       card.classList.add("book_card");
+      card.setAttribute("id", `${item}-${index}`);
+
       library.appendChild(card);
+
+      // Delete card & object in library
+      deleteBtn.addEventListener("click", (e) => {
+        for (let i = 0; i < myLibrary.length; i++) {
+          let index = myLibrary.indexOf([i]);
+          myLibrary.splice(index, 1);
+          // let nmb = card.getAttribute(`data-nmb`);
+          // console.log(nmb);
+          console.log(myLibrary);
+        }
+        document.getElementById(`${item}-${index}`).remove();
+      });
 
       // For every property in the object except 'displayed', create a paragraph containing it's value.
       for (let key in item) {
@@ -96,42 +118,42 @@ const displayBooks = function () {
         }
       }
 
-      // Create input for read/not read
-      const div = document.createElement("div");
-      const label = document.createElement("label");
-      const input = document.createElement("input");
+      // // Create input for read/not read
+      // const div = document.createElement("div");
+      // const label = document.createElement("label");
+      // const input = document.createElement("input");
 
-      input.setAttribute("type", "checkbox");
-      input.setAttribute("id", "isItChecked");
-      div.classList.add("flex_read");
-      label.classList.add("readLabel");
-      input.classList.add("readOrNot");
-      label.textContent = "Read?";
-      div.appendChild(label);
-      div.appendChild(input);
-      card.appendChild(div);
+      // input.setAttribute("type", "checkbox");
+      // input.setAttribute("id", "isItChecked");
+      // div.classList.add("flex_read");
+      // label.classList.add("readLabel");
+      // input.classList.add("readOrNot");
+      // label.textContent = "Read?";
+      // div.appendChild(label);
+      // div.appendChild(input);
+      // card.appendChild(div);
 
-      const isItRead = document.getElementById("isItChecked");
-      const readPara = document.createElement("p");
-      readPara.classList.add("readParagraph");
+      // // const isItRead = document.getElementById("isItChecked");
+      // const readPara = document.createElement("p");
+      // readPara.classList.add("readParagraph");
 
       // Check if checkbox is true or false and change paragraph content accordingly
-      isItRead.addEventListener("click", (e) => {
-        switch (true) {
-          case isItRead.checked === true:
-            console.log("Checked");
-            item.read = true;
-            readPara.textContent = "Finished";
-            card.appendChild(readPara);
-            break;
-          case isItRead.checked === false:
-            console.log("Not Checked");
-            item.read = false;
-            readPara.textContent = "Not finished";
-            card.appendChild(readPara);
-            break;
-        }
-      });
+      // isItRead.addEventListener("click", (e) => {
+      //   switch (true) {
+      //     case isItRead.checked === true:
+      //       console.log("Checked");
+      //       item.read = true;
+      //       readPara.textContent = "Finished";
+      //       card.appendChild(readPara);
+      //       break;
+      //     case isItRead.checked === false:
+      //       console.log("Not Checked");
+      //       item.read = false;
+      //       readPara.textContent = "Not finished";
+      //       card.appendChild(readPara);
+      //       break;
+      //   }
+      // });
     }
     // Change object's 'displayed' property to true, so it won't get displayed next time when function/loop is fired.
     item.displayed = true;
